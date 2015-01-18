2482c30ce158bd56bfb7dc70e81d6e051e99d60a
Paul Barry <mail@paulbarry.com>
2015-01-18 15:43:29 -0500

    Don't create dummy student enrollment

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,8 @@
 course = Course.create!(name: 'Back End Web Development', starts_on: '2014-01-21')
 
 paul = User.create!(first_name: 'Paul', last_name: 'Barry', email: 'mail@paulbarry.com', password: 'password', temporary_password: true)
-student = User.create!(first_name: 'Joe', last_name: 'Student', email: 'student@example.com', password: 'password', temporary_password: true)
 
 course.enrollments.create!(user: paul, role: 'instructor')
-course.enrollments.create!(user: student, role: 'student')
 
 course.lessons.create!(name: 'Mac OS X', description: %{This lesson will cover the basics of using the latest version of Mac OS X, 10.10 Yosemite}).tap do |lesson|
 
